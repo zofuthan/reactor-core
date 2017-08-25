89cc7797e473ef62b47408b590fc7fafcdb4780b
Simon Baslé <sbasle@pivotal.io>
2017-08-25 18:00:01 +0200

    adapt RxJava FlowableTimeoutTimed, needs FullArbiter (wip)

--- reactor-core/src/main/java/reactor/core/publisher/FluxTimeoutTimed.java
+++ reactor-core/src/main/java/reactor/core/publisher/FluxTimeoutTimed.java
@@ -17,29 +17,301 @@
 package reactor.core.publisher;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscription;
+import reactor.core.CoreSubscriber;
+import reactor.core.Disposable;
 import reactor.core.scheduler.Scheduler;
 
 /**
  * @author Simon Baslé
  * @author David Karnok
  */
-class FluxTimeoutTimed<T> extends FluxTimeoutOther<T, Long, Long> {
+class FluxTimeoutTimed<T> extends FluxOperator<T, T> {
 
-	public FluxTimeoutTimed(Flux<T> source, Duration timeout, Scheduler timer) {
-		super(source,
-				Mono.delay(timeout, timer).onErrorReturn(0L),
-				o -> Mono.delay(timeout, timer).onErrorReturn(0L)
-		);
+	static final Disposable NEW_TIMER = Disposable.disposed();
+
+	final Duration timeout;
+	final Scheduler timer;
+	final Publisher<? extends T> fallback;
+
+	public FluxTimeoutTimed(Flux<? extends T> source, Duration timeout, Scheduler timer) {
+		super(source);
+		this.timeout = timeout;
+		this.timer = timer;
+		this.fallback = null;
 	}
 
-	public FluxTimeoutTimed(Flux<T> source, Duration timeout, Scheduler timer,
+	public FluxTimeoutTimed(Flux<? extends T> source, Duration timeout, Scheduler timer,
 			Publisher<? extends T> fallback) {
-		super(source,
-				Mono.delay(timeout, timer).onErrorReturn(0L),
-				o -> Mono.delay(timeout, timer).onErrorReturn(0L),
-				fallback
-		);
+		super(source);
+		this.timeout = timeout;
+		this.timer = timer;
+		this.fallback = fallback;
 	}
+
+	@Override
+	public void subscribe(CoreSubscriber<? super T> s) {
+		if (fallback == null) {
+			source.subscribe(new TimeoutTimedSubscriber<T>(
+					new SerializedSubscriber<>(s), // because errors can race
+					timeout, timer.createWorker()));
+		} else {
+			source.subscribe(new TimeoutTimedOtherSubscriber<T>(
+					s, // the FullArbiter serializes
+					timeout, timer.createWorker(), fallback));
+		}
+	}
+
+	static final class TimeoutTimedOtherSubscriber<T> implements InnerOperator<T, T>, Disposable {
+		final CoreSubscriber<? super T> actual;
+		final Duration timeout;
+		final Scheduler.Worker worker;
+		final Publisher<? extends T> fallback;
+
+		Subscription s;
+
+		final FullArbiter<T> arbiter;
+
+		Disposable timer;
+
+		volatile long index;
+
+		volatile boolean done;
+
+		TimeoutTimedOtherSubscriber(CoreSubscriber<? super T> actual, Duration timeout, Scheduler.Worker worker,
+				Publisher<? extends T> fallback) {
+			this.actual = actual;
+			this.timeout = timeout;
+			this.worker = worker;
+			this.fallback = fallback;
+			this.arbiter = new FullArbiter<T>(actual, this, 8);
+		}
+
+		@Override
+		public void onSubscribe(Subscription s) {
+			if (Operators.validate(this.s, s)) {
+				this.s = s;
+				if (arbiter.setSubscription(s)) {
+					actual.onSubscribe(arbiter);
+
+					scheduleTimeout(0L);
+				}
+			}
+		}
+
+		@Override
+		public void onNext(T t) {
+			if (done) {
+				return;
+			}
+			long idx = index + 1;
+			index = idx;
+
+			if (arbiter.onNext(t, s)) {
+				scheduleTimeout(idx);
+			}
+		}
+
+		@Override
+		public CoreSubscriber<? super T> actual() {
+			return actual;
+		}
+
+		@Override
+		public void request(long l) {
+			//FIXME
+		}
+
+		@Override
+		public void cancel() {
+			s.cancel();
+			worker.dispose();
+		}
+
+		void scheduleTimeout(final long idx) {
+			if (timer != null) {
+				timer.dispose();
+			}
+
+			timer = worker.schedule(new TimeoutTask(idx), timeout.toMillis(), TimeUnit.MILLISECONDS);
+		}
+
+		void subscribeNext() {
+			fallback.subscribe(new FullArbiterSubscriber<>(arbiter));
+		}
+
+		@Override
+		public void onError(Throwable t) {
+			if (done) {
+				Operators.onErrorDropped(t);
+				return;
+			}
+			done = true;
+			arbiter.onError(t, s);
+			worker.dispose();
+		}
+
+		@Override
+		public void onComplete() {
+			if (done) {
+				return;
+			}
+			done = true;
+			arbiter.onComplete(s);
+			worker.dispose();
+		}
+
+		@Override
+		public void dispose() {
+			cancel();
+		}
+
+		@Override
+		public boolean isDisposed() {
+			return worker.isDisposed();
+		}
+
+		final class TimeoutTask implements Runnable {
+			private final long idx;
+
+			TimeoutTask(long idx) {
+				this.idx = idx;
+			}
+
+			@Override
+			public void run() {
+				if (idx == index) {
+					done = true;
+					s.cancel();
+					worker.dispose();
+
+					subscribeNext();
+
+				}
+			}
+		}
+	}
+
+	static final class TimeoutTimedSubscriber<T> implements InnerOperator<T, T>, Disposable, Subscription {
+		final CoreSubscriber<? super T> actual;
+		final Duration timeout;
+		final Scheduler.Worker worker;
+
+		Subscription s;
+
+		Disposable timer;
+
+		volatile long index;
+
+		volatile boolean done;
+
+		TimeoutTimedSubscriber(CoreSubscriber<? super T> actual, Duration timeout, Scheduler.Worker worker) {
+			this.actual = actual;
+			this.timeout = timeout;
+			this.worker = worker;
+		}
+
+		@Override
+		public void onSubscribe(Subscription s) {
+			if (Operators.validate(this.s, s)) {
+				this.s = s;
+				actual.onSubscribe(this);
+				scheduleTimeout(0L);
+			}
+		}
+
+		@Override
+		public void onNext(T t) {
+			if (done) {
+				Operators.onNextDropped(t);
+				return;
+			}
+			long idx = index + 1;
+			index = idx;
+
+			actual.onNext(t);
+
+			scheduleTimeout(idx);
+		}
+
+		void scheduleTimeout(final long idx) {
+			if (timer != null) {
+				timer.dispose();
+			}
+
+			timer = worker.schedule(new TimeoutTask(idx), timeout.toMillis(), TimeUnit.MILLISECONDS);
+		}
+
+		@Override
+		public void onError(Throwable t) {
+			if (done) {
+				Operators.onErrorDropped(t);
+				return;
+			}
+			done = true;
+
+			actual.onError(t);
+			worker.dispose();
+		}
+
+		@Override
+		public void onComplete() {
+			if (done) {
+				return;
+			}
+			done = true;
+
+			actual.onComplete();
+			worker.dispose();
+		}
+
+		@Override
+		public CoreSubscriber<? super T> actual() {
+			return actual;
+		}
+
+		@Override
+		public void dispose() {
+			s.cancel();
+			worker.dispose();
+		}
+
+		@Override
+		public boolean isDisposed() {
+			return worker.isDisposed();
+		}
+
+		@Override
+		public void request(long n) {
+			s.request(n);
+		}
+
+		@Override
+		public void cancel() {
+			dispose();
+		}
+
+		final class TimeoutTask implements Runnable {
+			private final long idx;
+
+			TimeoutTask(long idx) {
+				this.idx = idx;
+			}
+
+			@Override
+			public void run() {
+				if (idx == index) {
+					done = true;
+					dispose();
+
+					actual.onError(new TimeoutException());
+				}
+			}
+		}
+	}
+
 }
---
+++ reactor-core/src/main/java/reactor/core/publisher/FullArbiter.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright (c) 2016-present, RxJava Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package reactor.core.publisher;
+
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+import java.util.function.BiConsumer;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.Disposable;
+import reactor.util.concurrent.Queues;
+
+/**
+ * Performs full arbitration of Subscriber events with strict drain (i.e., old emissions of another
+ * subscriber are dropped).
+ *
+ * @param <T> the value type
+ * @author David Karnok
+ */
+public final class FullArbiter<T> extends FullArbiterPad2 implements Subscription {
+
+	final Subscriber<? super T>         actual;
+	final BiConsumer<Object, Object>    queue;
+
+	long requested;
+
+	volatile Subscription s;
+	static final Subscription INITIAL = new InitialSubscription();
+
+	Disposable resource;
+
+	volatile boolean cancelled;
+
+	static final Object REQUEST = new Object();
+
+	public FullArbiter(Subscriber<? super T> actual, Disposable resource, int capacity) {
+		this.actual = actual;
+		this.resource = resource;
+		Queue<Object> q = Queues.unbounded(capacity).get();
+		assert(q instanceof BiConsumer);
+		//noinspection unchecked
+		this.queue = (BiConsumer<Object, Object>) q;
+		this.s = INITIAL;
+	}
+
+	@Override
+	public void request(long n) {
+		if (Operators.validate(n)) {
+			Operators.getAndAddCap(MISSED_REQUESTED, this, n);
+			queue.accept(REQUEST, REQUEST);
+			drain();
+		}
+	}
+
+	@Override
+	public void cancel() {
+		if (!cancelled) {
+			cancelled = true;
+			dispose();
+		}
+	}
+
+	void dispose() {
+		Disposable d = resource;
+		resource = null;
+		if (d != null) {
+			d.dispose();
+		}
+	}
+
+	public boolean setSubscription(Subscription s) {
+		if (cancelled) {
+			if (s != null) {
+				s.cancel();
+			}
+			return false;
+		}
+
+		Objects.requireNonNull(s, "s is null");
+		queue.accept(this.s, Signal.subscribe(s));
+		drain();
+		return true;
+	}
+
+	public boolean onNext(T value, Subscription s) {
+		if (cancelled) {
+			return false;
+		}
+
+		queue.accept(s, Signal.next(value));
+		drain();
+		return true;
+	}
+
+	public void onError(Throwable value, Subscription s) {
+		if (cancelled) {
+			Operators.onErrorDropped(value);
+			return;
+		}
+		queue.accept(s, Signal.error(value));
+		drain();
+	}
+
+	public void onComplete(Subscription s) {
+		queue.accept(s, Signal.complete());
+		drain();
+	}
+
+	void drain() {
+		if (wip.getAndIncrement() != 0) {
+			return;
+		}
+
+		int missed = 1;
+
+		//noinspection unchecked
+		final Queue<Object> q = (Queue<Object>) queue;
+		final Subscriber<? super T> a = actual;
+
+		for (;;) {
+
+			for (;;) {
+
+				Object o = q.poll();
+				if (o == null) {
+					break;
+				}
+				Object v = q.poll();
+
+				if (o == REQUEST) {
+					long mr = MISSED_REQUESTED.getAndSet(this,0L);
+					if (mr != 0L) {
+						requested = Operators.addCap(requested, mr);
+						s.request(mr);
+					}
+				} else
+				if (o == s) {
+					Signal<T> signal;
+					if (!(v instanceof Signal)) {
+						signal = Signal.error(new IllegalStateException("While polling, expected a Signal but found " + v));
+					}
+					else {
+						//noinspection unchecked
+						signal = (Signal<T>) v;
+					}
+
+					if (signal.isOnSubscribe()) {
+						Subscription next = signal.getSubscription();
+						if (!cancelled) {
+							s = next;
+							long r = requested;
+							if (r != 0L) {
+								next.request(r);
+							}
+						} else {
+							next.cancel();
+						}
+					} else if (signal.isOnError()) {
+						q.clear();
+						dispose();
+
+						Throwable ex = signal.getThrowable();
+						if (!cancelled) {
+							cancelled = true;
+							a.onError(ex);
+						} else {
+							Operators.onErrorDropped(ex);
+						}
+					} else if (signal.isOnComplete()) {
+						q.clear();
+						dispose();
+
+						if (!cancelled) {
+							cancelled = true;
+							a.onComplete();
+						}
+					} else {
+						long r = requested;
+						if (r != 0) {
+							a.onNext(signal.get());
+							requested = r - 1;
+						}
+					}
+				}
+			}
+
+			missed = wip.addAndGet(-missed);
+			if (missed == 0) {
+				break;
+			}
+		}
+	}
+
+	static final class InitialSubscription implements Subscription {
+		@Override
+		public void request(long n) {
+			// deliberately no op
+		}
+
+		@Override
+		public void cancel() {
+			// deliberately no op
+		}
+	}
+}
+
+/** Pads the object header away. */
+class FullArbiterPad0 {
+	volatile long p1a, p2a, p3a, p4a, p5a, p6a, p7a;
+	volatile long p8a, p9a, p10a, p11a, p12a, p13a, p14a, p15a;
+}
+
+/** The work-in-progress counter. */
+class FullArbiterWip extends FullArbiterPad0 {
+	final AtomicInteger wip = new AtomicInteger();
+}
+
+/** Pads the wip counter away. */
+class FullArbiterPad1 extends FullArbiterWip {
+	volatile long p1b, p2b, p3b, p4b, p5b, p6b, p7b;
+	volatile long p8b, p9b, p10b, p11b, p12b, p13b, p14b, p15b;
+}
+
+/** The missed request counter. */
+class FullArbiterMissed extends FullArbiterPad1 {
+	volatile long missedRequested;
+	static AtomicLongFieldUpdater<FullArbiterMissed> MISSED_REQUESTED =
+			AtomicLongFieldUpdater.newUpdater(FullArbiterMissed.class, "missedRequested");
+}
+
+/** Pads the missed request counter away. */
+class FullArbiterPad2 extends FullArbiterMissed {
+	volatile long p1c, p2c, p3c, p4c, p5c, p6c, p7c;
+	volatile long p8c, p9c, p10c, p11c, p12c, p13c, p14c, p15c;
+}
